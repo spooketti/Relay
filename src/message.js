ebd05c5317263163cf9ba6022fa70835acd431c1
spooketti <jonliu168@gmail.com>
2024-05-06 21:50:06 -0700

    url highlighting

--- src/message.js
+++ src/message.js
@@ -1,6 +1,7 @@
 const queryMessageEndpoint = "https://relayserver-5l9m.onrender.com/getServerMessage/"//"http://127.0.0.1:6221/getServerMessage/"
 let MessageBody = document.getElementById("MessageBody")
 let ChannelNavName = document.getElementById('ChannelNameNav')
+let urlRegex = /(https?:\/\/[^\s]+)/g
 
 function queryMessage(specialScroll) {
     let payload =
@@ -65,7 +66,8 @@ function appendMessage(data, isNew) {
     MessageWrapper.dataset.datesec = data["date"]
     MessageDate.innerText = dateTime(data["date"])
     let MessageContent = document.createElement("span")
-    MessageContent.innerText = data["message"]
+    // MessageContent.innerText = data["message"]
+    urlSplit(data["message"])
     MessageContent.classList.add("MessageContent")
     MessageWrapper.dataset.userTableID = data["userID"]
 
@@ -77,6 +79,40 @@ function appendMessage(data, isNew) {
         MessageUsername.appendChild(MessageDate)
         MessageUsername.appendChild(document.createElement("br"))
         MessageUsername.appendChild(MessageContent)
+    }
+
+    function urlSplit(input) {
+        let urlRegex = /(?:https?|ftp):\/\/[^\s/$.?#].[^\s]*/gi;
+        let matches = input.match(urlRegex);
+        if (!matches) {
+            let beforeURL = document.createElement("span")
+            beforeURL.innerText = input
+            MessageContent.appendChild(beforeURL)
+            return
+        }
+        let lastIndex = 0;
+        matches.forEach(function (url) {
+            let index = input.indexOf(url, lastIndex);
+            if (index > lastIndex) { //if its before the url
+                let beforeURL = document.createElement("span")
+                beforeURL.innerText = input.substring(lastIndex, index)
+                MessageContent.appendChild(beforeURL)
+            }
+            let urlAnchor = document.createElement("a")
+            urlAnchor.href = url
+            urlAnchor.innerText = url
+            urlAnchor.target = "_blank"
+            urlAnchor.rel = "noopener"
+            urlAnchor.style.color = "#429eed"
+            MessageContent.appendChild(urlAnchor)
+            lastIndex = index + url.length;
+        });
+
+        if (lastIndex < input.length) {
+            let leftOver = document.createElement("span")
+            leftOver.innerText = input.substring(lastIndex)
+            MessageContent.appendChild(leftOver) //if its after url
+        }
     }
 
     if (isNew) {
@@ -130,7 +166,7 @@ function appendMessage(data, isNew) {
         }
         appendFullMessage()
         MessageBody.prepend(MessageWrapper) //if this is an old message query it at the top as thats where the old messages are
-    
+
     }
 }
 
